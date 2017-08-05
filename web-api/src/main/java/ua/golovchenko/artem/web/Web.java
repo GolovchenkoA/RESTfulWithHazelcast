72370e98b834422d7858ac8d5de3206dc7fc184d
Golovchenko Artem <golovchenkoart@gmail.com>
2017-08-05 12:33:03 +0300

    промежуточный Web-api  2

--- web-api/src/main/java/ua/golovchenko/artem/web/Web.java
+++ web-api/src/main/java/ua/golovchenko/artem/web/Web.java
@@ -1,26 +1,30 @@
 package ua.golovchenko.artem.web;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 /**
  * Created by головченко on 03.08.2017.
  */
-public class Main {
+public class Web {
+    public static final int PORT = 80;
+
 
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create();
-        server.bind(new InetSocketAddress(80), 0);
-        server.createContext("/", new MyHandler());
+/*        HttpServer server = HttpServer.create(new InetSocketAddress(PORT));
+        server.createContext("/", new BaseHandler());
+        server.start();*/
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT),0);
+        server.createContext("/", new BaseHandler());
+        server.createContext("/useringo", new UserHandler());
+        server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+/*    static class BaseHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "This is the response 2";
@@ -29,5 +33,6 @@ public class Main {
             os.write(response.getBytes());
             os.close();
         }
-    }
+    }*/
+
 }
