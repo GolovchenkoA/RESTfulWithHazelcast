03db494c900a095a5d4c8598df4e4763c229b59f
Golovchenko Artem <golovchenkoart@gmail.com>
2017-08-23 22:51:55 +0300

    вынесь код в отдельные методы

--- service/src/main/java/ua/golovchenko/artem/game/cache/CacheResultService.java
+++ service/src/main/java/ua/golovchenko/artem/game/cache/CacheResultService.java
@@ -2,9 +2,6 @@ package ua.golovchenko.artem.game.cache;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ua.golovchenko.artem.game.cache.dao.CacheResultDAO;
-import ua.golovchenko.artem.game.dao.DataManager;
-import ua.golovchenko.artem.game.dao.ResultDAO;
 import ua.golovchenko.artem.game.service.ResultService;
 import ua.golovchenko.artem.model.Result;
 import ua.golovchenko.artem.model.User;
@@ -18,45 +15,54 @@ import java.util.Collections;
  */
 public class CacheResultService implements ResultService {
     private static final int MAX_RESULTS_COUNT = 3;
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    DataManager dataManager = new DataManager();
-    ResultDAO resultDAO = new CacheResultDAO();
-    CacheUserService userService = new CacheUserService();
-    CacheLevelService levelService = new CacheLevelService();
+    private final Logger logger     = LoggerFactory.getLogger(this.getClass());
+    CacheUserService userService    = new CacheUserService();
+    CacheLevelService levelService  = new CacheLevelService();
 
 
     @Override
     public void add(Result result) throws Exception {
-        Long user_id = result.getUser_id();
-        User user = userService.get(user_id);
-        if(user == null){
-            logger.debug("User with id [{}] does not exists. Create new user ", user_id);
-            user = userService.generateNewUser(user_id);
-            userService.add(user);
-        }
+        Long user_id =  result.getUser_id();
+        User user =  userService.get(user_id);
+        user = this.createUserIfNotExists(user,user_id);
 
+        this.checkMaximumNumberOfResultsAllowed(user, MAX_RESULTS_COUNT);
+        logger.debug("Add new result [{}] to user with id [{}] .start ", user_id, result);
+        user.getResults().add(result);
+        this.updateCacheCollections(user,result);
+    }
 
+    private void checkMaximumNumberOfResultsAllowed(User user, int maximum_number) {
         int items_cont;
+
         try{
             items_cont = user.getResults().size();
         }catch (Exception e){
-            logger.debug("User [id: {}] have no results.",user_id);
+            logger.debug("User [id: {}] have no results.",user.getUser_id());
             items_cont = 0;
         }
 
         if(items_cont >= MAX_RESULTS_COUNT){
             Collections.sort(user.getResults());
-            logger.debug("User with id [{}] have maximum results count [{}]. removing item", user_id,MAX_RESULTS_COUNT);
+            logger.debug("User with id [{}] have maximum results count [{}]. removing item", user.getUser_id(),maximum_number);
             user.getResults().subList(MAX_RESULTS_COUNT, items_cont).clear();
         }
 
-        logger.debug("Add new result [{}] to user with id [{}] .start ", user_id, result);
-        user.getResults().add(result);
+    }
 
+    private User createUserIfNotExists(User user, Long user_id) throws Exception {
+        if(user == null){
+            logger.debug("User with id [{}] does not exists. Create new user ", user_id);
+            user = userService.generateNewUser(user_id);
+            userService.add(user);
+        }
+        return user;
+    }
 
+    private void updateCacheCollections(User user, Result result) throws Exception {
         try {
             userService.update(user);
-            logger.debug("UserService. Add new result [{}] to user with id [{}] . finish", user_id, result);
+            logger.debug("UserService. Add new result [{}] to user with id [{}] . finish", user.getUser_id(), result);
         } catch (Exception e) {
             logger.debug("Error update user. StackTrace {}", e);
             throw new Exception(e);
@@ -65,11 +71,10 @@ public class CacheResultService implements ResultService {
         //Update Results in Level-MultiMap
         try{
             levelService.update(user);
-            logger.debug("LevelService. Add new result [{}] to user with id [{}] . finish", user_id, result);
+            logger.debug("LevelService. Add new result [{}] to user with id [{}] . finish", user.getUser_id(), result);
         } catch (Exception e) {
             logger.debug("Error update user. StackTrace {}", e);
             throw new Exception(e);
         }
-
     }
 }
